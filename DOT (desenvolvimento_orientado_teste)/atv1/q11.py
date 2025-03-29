aff6673d998a963efb99bb7b9f46f7f3e14b5d1e
luis henrique <luis333joao333@gmail.com>
2025-03-29 17:10:14 -0300

    update 0.1

--- DOT (desenvolvimento_orientado_teste)/atv1/q11.py
+++ DOT (desenvolvimento_orientado_teste)/atv1/q11.py
@@ -1,2 +1,24 @@
 # questão 11 dot
 #Faça uma função que recebe, por parâmetro, um valor inteiro e positivo e retorna o número de divisores desse valor.
+
+def divisore(num):
+    lista_div = []
+    for i in range(1,num+1):
+        if num % i == 0:
+            lista_div.append(i)
+
+    return lista_div
+
+
+while True:
+    try:
+        num = int(input('Digite um número : '))
+        if num > 0:
+            resul = divisore(num)
+            print(f'seu número {num} seu divisores : {resul}')
+            break
+        else:
+            print('valor tem que ser positivo.Digite novamente : ')
+
+    except ValueError:
+        print('valor invalidor')
