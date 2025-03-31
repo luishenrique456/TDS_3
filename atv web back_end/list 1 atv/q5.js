218d43e58e38216747329013af95cc3b68a7a0da
luis henrique <luis333joao333@gmail.com>
2025-03-31 09:53:42 -0300

    up 0.2

--- atv web back_end/list 1 atv/q5.js
+++ atv web back_end/list 1 atv/q5.js
@@ -4,3 +4,25 @@
 // const numeros = [3, 7, 3, 2, 7, 10, 2, 15, 10, 20];
 
 const numeros = [3, 7, 3, 2, 7, 10, 2, 15, 10, 20];
+
+const novo_numeros = [];
+
+for(let i=0;i< numeros.length;i++){
+    if(verficarRepertido(numeros[i])){
+        console.log("número " + numeros[i] + " repertido")
+    }
+    else{
+        novo_numeros.push(numeros[i]);
+    }
+}
+
+function verficarRepertido(numero){
+    for(let i = 0 ; i< novo_numeros.length;i++){
+        if(numero === novo_numeros[i]){
+            return true
+        }
+    }
+    return false
+}
+
+console.log(novo_numeros)
