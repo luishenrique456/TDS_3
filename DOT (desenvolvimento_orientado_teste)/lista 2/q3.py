3ad567ea74f2a9d1c393f691716178cfb9a9617a
luis henrique <luis333joao333@gmail.com>
2025-04-01 21:39:40 -0300

    primeiro commit

---
+++ DOT (desenvolvimento_orientado_teste)/lista 2/q3.py
@@ -0,0 +1,2 @@
+# 3) Faça um programa que dada uma seqüência de n números, imprimi-la na ordem inversa à da
+# leitura.
---
+++ DOT (desenvolvimento_orientado_teste)/lista 2/q8.py
@@ -0,0 +1,3 @@
+# 8) Dada uma lista contendo letras do alfabeto, elabore um programa para verificar quantas vezes
+# ocorreu a letra ‘A’.
+# OBS: Fazer crítica na entrada do caractere para aceitar somente letras.
