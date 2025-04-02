ce640659111fef8b6750c169fc2d2716e6463793
luis henrique <luis333joao333@gmail.com>
2025-04-02 12:13:08 -0300

    up

--- atv web back_end/list 1 atv/q6.js
+++ atv web back_end/list 1 atv/q6.js
@@ -1,2 +1,12 @@
 // Implemente uma função que recebe como parâmetro um array de números, calcule
 // a média aritmética e retorne o resultado.
+const numeros = [3, 7, 3, 2, 7, 10, 2, 15, 10, 20];
+
+function calcular_media(numeros){
+    for(let i = 0;i>numeros.length;i++){
+        
+    }
+}
+
+
+console.log(calcular_media())
