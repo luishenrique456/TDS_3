ef403d1c42d694e952d3c1193864022694b0a4a5
luis henrique <luis333joao333@gmail.com>
2025-05-06 10:31:10 -0300

    up 0.4

--- DOT (desenvolvimento_orientado_teste)/lista 2 dot teste/q8_lista2.py
+++ DOT (desenvolvimento_orientado_teste)/lista 2 dot teste/q8_lista2.py
@@ -3,7 +3,7 @@
 # OBS: Fazer crítica na entrada do caractere para aceitar somente letras.
 
 def verficar_letra_A(caractere):
-    if type(caractere) != str:
+    if type(caractere) != str or 'B' <= caractere.upper() <= 'Z':
         return Exception
 
     cont = 0
@@ -11,10 +11,23 @@ def verficar_letra_A(caractere):
     for caractere in lista_letras:
         if caractere == 'A':
             cont +=1
+        
     return cont 
 
 assert verficar_letra_A('A') == 2
 
 assert verficar_letra_A(1) == Exception
+
+assert verficar_letra_A(3.0) == Exception
+
+assert verficar_letra_A([2]) == Exception
+
+assert verficar_letra_A('B') == Exception
+
+assert verficar_letra_A('C') == Exception
+
+assert verficar_letra_A('R') == Exception
+
+assert verficar_letra_A('r') == Exception
 
 print('teste tudo OK! da questão 8 da lista 2')
--- DOT (desenvolvimento_orientado_teste)/lista 2 dot teste/q9_lista2.py
+++ DOT (desenvolvimento_orientado_teste)/lista 2 dot teste/q9_lista2.py
@@ -1,10 +1,22 @@
 # 9) Dada uma lista X numérica contendo 5 elementos, fazer um programa que crie e exiba na tela
 # uma lista Y. A lista Y deverá conter o mesmo conteúdo da lista X na ordem inversa.
 def inverter(listas_num):
+    if type(listas_num) != list or len(listas_num) == 0:
+        return Exception
     listas_num.reverse()
     return listas_num
 
 
 assert inverter([8,7,6,5,4,3,2,1]) == [1,2,3,4,5,6,7,8]
+
+# verficar entrada do parâmetro é do tipo list
+
+assert inverter(1) == Exception
+
+assert inverter(3.0) == Exception
+
+assert inverter('a') == Exception
+
+assert inverter([]) == Exception
 
 print('Teste tudo Ok! da questão 9 da lista 2')
