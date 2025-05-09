728ac01d7fa27671c1a26005e50ef838b879d261
luis henrique <luis333joao333@gmail.com>
2025-05-09 19:59:39 -0300

    up 0.1

--- DOT (desenvolvimento_orientado_teste)/lista 3 dot teste/q1_lista3.py
+++ DOT (desenvolvimento_orientado_teste)/lista 3 dot teste/q1_lista3.py
@@ -4,26 +4,49 @@
 # from random import randint
 
 
-def gera_lista(n):
-    lista_num = []
-    for i in range(n):
-        num = int(input('Digite um número da lista : '))
+# def gera_lista(n):
+#     lista_num = []
+#     for i in range(n):
+#         num = int(input('Digite um número da lista : '))
 
-        lista_num.append(num)
+#         lista_num.append(num)
 
-        # lista_num.append(randint(1,5))
+#         # lista_num.append(randint(1,5))
 
-    return lista_num
+#     return lista_num
 
 
 # print(gera_lista(4))
 
 def del_numeros_rep(lista_num):
-    pass
+    if type(lista_num) != list or not all(isinstance(i,int)for i in lista_num) or len(lista_num) == 0:
+        return Exception
+    lista_new = []
+    for i in lista_num:
+        if i not in lista_new:
+            lista_new.append(i)
+
+    return lista_new
+    
 
 
 
 
-assert gera_lista(4) == [1,2,3,4]
+
+# assert gera_lista(4) == [1,2,3,4]
+
+assert del_numeros_rep([5,4,5,7,3,4]) == [5,4,7,3]
+
+assert del_numeros_rep(3) == Exception
+
+assert del_numeros_rep(4.6) == Exception
+
+assert del_numeros_rep('a') == Exception
+
+assert del_numeros_rep(['a']) == Exception
+
+assert del_numeros_rep([2.4]) == Exception
+
+assert del_numeros_rep([]) == Exception
 
 print('teste tudo OK! da questão 1 da lista 3')
--- DOT (desenvolvimento_orientado_teste)/lista 3 dot teste/q2_lista3.py
+++ DOT (desenvolvimento_orientado_teste)/lista 3 dot teste/q2_lista3.py
@@ -1,2 +1,60 @@
 # 2) Escreva uma função que recebe uma lista com n números inteiros, conta e imprime
 # o número de vezes que cada número ocorre na sequência.
+
+def conta_itens_list(lista_numeros,item):
+    if type(lista_numeros) != list or not all(isinstance(i,int)for i in lista_numeros) or len(lista_numeros) == 0:
+        return Exception
+    if type(item) != int:
+        return Exception
+    ocorrecia = 0
+    for i in lista_numeros:
+        if i == item:
+            ocorrecia += 1
+    return ocorrecia
+
+def tira_numero_rep(lista_num):
+    if type(lista_num) != list or not all(isinstance(i,int)for i in lista_num) or len(lista_num) == 0:
+        return Exception
+    
+    lista_new = []
+    for i in lista_num:
+        if i not in lista_new:
+            lista_new.append(i)
+
+    return lista_new
+
+def qtd_cada_itens_list(lista_numeros,numeros):
+    if type(lista_numeros) != list or not all(isinstance(i,int)for i in lista_numeros) or len(lista_numeros) == 0:
+        return Exception
+    elif type(numeros) != list or not all(isinstance(i,int)for i in numeros) or len(numeros) == 0:
+        return Exception
+    lista_resultado = []
+    for i in numeros:
+        qtd = conta_itens_list(lista_numeros,i)
+        lista_resultado.append(f'numero {i} sequência {qtd}')
+
+    return lista_resultado
+
+
+
+
+
+assert conta_itens_list([1,2,3,2,4,4],2) == 2
+
+assert tira_numero_rep([1,2,3,2,4,4]) == [1,2,3,4]
+
+assert qtd_cada_itens_list([1,2,3,2,4,4],[1,2,3,4]) == ['numero 1 sequência 1','numero 2 sequência 2','numero 3 sequência 1','numero 4 sequência 2']
+
+assert conta_itens_list('a',2) == Exception
+
+assert conta_itens_list([2],2.0) == Exception
+
+assert tira_numero_rep(['a']) == Exception
+
+assert tira_numero_rep(2.0) == Exception
+
+assert qtd_cada_itens_list('a',[2]) == Exception
+
+assert qtd_cada_itens_list([1],'a') == Exception
+
+print('teste tudo OK! da questão 2 da lista 3')
